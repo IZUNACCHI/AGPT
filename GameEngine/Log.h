db0a0d1e4347415139009bcc5c045ffaffdf84ab
Afonso <2231864@my.ipleiria.pt>
2025-11-30 09:29:51 +0000

    Log comments

--- GameEngine/Log.h
+++ GameEngine/Log.h
@@ -11,16 +11,16 @@ enum class LogLevel {
 
 class Log {
 public:
-	// The one function to rule them all - just like you wanted!
+	// General log function
 	static void log(LogLevel level, std::string_view message);
 
-	// Nice shortcuts (optional but convenient)
+	// shortcuts
 	static void info(std::string_view msg) { log(LogLevel::Info, msg); }
 	static void warning(std::string_view msg) { log(LogLevel::Warning, msg); }
 	static void error(std::string_view msg) { log(LogLevel::Error, msg); }
 	static void debug(std::string_view msg) { log(LogLevel::Debug, msg); }
 
-	// Control debug logging
+	// Debug on/off
 	static void SetDebugEnabled(bool enabled) { showDebug = enabled; }
 	static bool IsDebugEnabled() { return showDebug; }
 
